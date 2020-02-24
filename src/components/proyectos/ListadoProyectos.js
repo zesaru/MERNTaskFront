928359787d6d9d9058fc28c69dc5ff8b0c625731
Cesar Murillo <namiayumis@gmail.com>
2020-02-24 18:51:47 +0900

    Llamando los Proyectos con dispatch

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Proyecto from "./Proyecto";
 import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const ListadoProyectos = () => {
   // Extrar proyectos de state inicial
   const proyectosContext = useContext(proyectoContext);
-  const { proyectos } = proyectosContext;
+  const { proyectos, obtenerProyectos } = proyectosContext;
+
+  // Obtener proyectos cuando carga el componente
+  useEffect(() => {
+    obtenerProyectos();
+  }, [obtenerProyectos]);
 
   // revisar si proyectos tiene contenido
   if (proyectos.length === 0) return null;
--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -2,16 +2,18 @@ import React, { useReducer } from "react";
 
 import proyectoContext from "./proyectoContext";
 import proyectoReducer from "./proyectoReducer";
-import { FORMULARIO_PROYECTO } from "../../types";
+import { FORMULARIO_PROYECTO, OBTENER_PROYECTOS } from "../../types";
 
 const ProyectoState = props => {
+  const proyectos = [
+    { id: 1, nombre: "Tienda Virtual" },
+    { id: 2, nombre: "Intranet" },
+    { id: 3, nombre: "Diseño de Sitio web" },
+    { id: 4, nombre: "MERN " }
+  ];
+
   const initialState = {
-    proyectos: [
-      { id: 1, nombre: "Tienda Virtual" },
-      { id: 2, nombre: "Intranet" },
-      { id: 3, nombre: "Diseño de Sitio web" },
-      { id: 4, nombre: "MERN " }
-    ],
+    proyectos: [],
     formulario: true
   };
 
@@ -24,12 +26,21 @@ const ProyectoState = props => {
       type: FORMULARIO_PROYECTO
     });
   };
+
+  // Obtener los proyectos
+  const obtenerProyectos = () => {
+    dispatch({
+      type: OBTENER_PROYECTOS,
+      payload: proyectos
+    });
+  };
   return (
     <proyectoContext.Provider
       value={{
         proyectos: state.proyectos,
         formulario: state.formulario,
-        mostrarFormulario
+        mostrarFormulario,
+        obtenerProyectos
       }}
     >
       {props.children}

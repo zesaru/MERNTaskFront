72d73c35bb9e37c8665b3278f32e65919c2c694c
Cesar Murillo <namiayumis@gmail.com>
2020-02-27 10:50:36 +0900

    Creando una función que obtenga las tareas del Proyecto

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,16 +1,26 @@
 import React, { useContext } from "react";
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
 
 const Proyecto = ({ proyecto }) => {
   // Obtener el state de proyectos
   const proyectosContext = useContext(proyectoContext);
   const { proyectoActual } = proyectosContext;
+
+  //obtener la funcion del contex de tarea
+  const tareasContext = useContext(tareaContext);
+  const { obtenerTareas } = tareaContext();
+  //Funcion para agregar el proyecto actual
+  const seleccionarProyecto = id => {
+    proyectoActual(id); // Fijar proyecto actual
+    obtenerTareas(id); // Filtrar las tareas cuando se de clic
+  };
   return (
     <li key={proyecto.id}>
       <button
         type="button"
         className="btn btn-blank"
-        onClick={() => proyectoActual(proyecto.id)}
+        onClick={() => seleccionarProyecto(proyecto.id)}
       >
         {proyecto.nombre}
       </button>
--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,10 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
 
 const FormTarea = () => {
   // Extrar proyectos de state inicial
   const proyectosContext = useContext(proyectoContext);
   const { proyecto } = proyectosContext;
+
+  // obtener la función del context de tarea
+  const tareasContext = useContext(tareaContext);
+  const { agregarTarea } = tareasContext;
+
+  // State del formulario
+  const [tarea, guardarTarea] = useState({
+    nombre: ""
+  });
+
+  // extraer el nombre del proyecto
+  const { nombre } = tarea;
 
   // Si no hay proyectado seleccionado
   if (!proyecto) return null;

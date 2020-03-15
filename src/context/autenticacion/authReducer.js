f773d44f5dbc0bfeabab97621c9d05bd435fd346
Cesar Murillo <namiayumis@gmail.com>
2020-03-15 18:59:24 +0900

    obtener usuario en api y action

--- src/context/autenticacion/authReducer.js
+++ src/context/autenticacion/authReducer.js
@@ -16,8 +16,9 @@ export default (state, action) => {
         autenticado: true,
         mensaje: null
       };
-
+    case LOGIN_ERROR:
     case REGISTRO_ERROR:
+      localStorage.removeItem("token");
       return {
         ...state,
         token: null,
